2f701977b3794a1e22c4ae30bc5a5d1ace048d20
Roberto Castro Valenzuela <roberto.castrovalenzuela@ucr.ac.cr>
2019-11-02 12:45:54 -0600

    Project declutter

--- JavaBackend/devops/start_engine.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Start Engine Script
-
-echo "Starting Engine from script"
-java -classpath ../lib/Engine.jar backend.app.Engine
-
-exit 0
--- JavaBackend/src/backend/app/Engine.java
+++
@@ -1,9 +0,0 @@
-package backend.app;
-
-public class Engine{
-	public static void main(String[] args){
-		System.out.println("Engine Start");
-		// Engine loop
-		System.out.println("Engine Shutting down");
-	}
-}
