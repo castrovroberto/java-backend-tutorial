1f657a9962b773f717dc8f6b6535b2fc3edac4c5
Roberto Castro Valenzuela <roberto.castrovalenzuela@ucr.ac.cr>
2019-11-02 12:40:03 -0600

    Initial commit

---
+++ JavaBackend/devops/start_engine.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Start Engine Script
+
+echo "Starting Engine from script"
+java -classpath ../lib/Engine.jar backend.app.Engine
+
+exit 0
---
+++ JavaBackend/src/backend/app/Engine.java
@@ -0,0 +1,9 @@
+package backend.app;
+
+public class Engine{
+	public static void main(String[] args){
+		System.out.println("Engine Start");
+		// Engine loop
+		System.out.println("Engine Shutting down");
+	}
+}
